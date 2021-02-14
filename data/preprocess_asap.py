79874fdc5cb5f6809ea268d9fb7cb243f9dd480d
yyoo@beta <yyoo@beta>
2021-02-14 23:28:24 +0900

    converted to python 3 by 2to3

--- data/preprocess_asap.py
+++ data/preprocess_asap.py
@@ -18,7 +18,7 @@ def extract_based_on_ids(dataset, id_file):
 			try:
 				lines.append(dataset[id])
 			except:
-				print >> sys.stederr, 'ERROR: Invalid ID %s in %s' % (id, id_file)
+				print('ERROR: Invalid ID %s in %s' % (id, id_file), file=sys.stederr)
 	return lines
 
 def create_dataset(lines, output_fname):
@@ -42,7 +42,7 @@ def collect_dataset(input_file):
 	return dataset
 
 dataset = collect_dataset(args.input_file)
-for fold_idx in xrange(0, 5):
+for fold_idx in range(0, 5):
 	for dataset_type in ['dev', 'test', 'train']:
 		lines = extract_based_on_ids(dataset, 'fold_%d/%s_ids.txt' % (fold_idx, dataset_type))
 		create_dataset(lines, 'fold_%d/%s.tsv' % (fold_idx, dataset_type))
